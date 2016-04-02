4aaf161902d70d2c7eb64d7d6e23dbcfe1fe0ce3
Katrin Leinweber <katrinleinweber@users.noreply.github.com>
2016-04-02 14:52:17 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: doi-resolver
-# about: replace DOI:<DOI> with DOI:http://dx.doi.org/<DOI>
+# about: replace DOI:<DOI> with DOI:https://doi.org/<DOI>
 # version: 0.6
 # authors: Jay Pfaffman
 
